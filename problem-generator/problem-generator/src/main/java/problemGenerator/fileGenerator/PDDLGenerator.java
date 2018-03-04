018ce75133dcf597136dfba956f04d880b35e79a
dan <dan@dan-VirtualBox>
2018-03-05 00:06:46 +0200

    initial commit for problem-generator project:
    * focuses on the logistics00 domain only!
    
    	- has addl + pddl file generated in the corresponding folders
    (./gen/addl + ./gen pddl).
    
    	- files are names by the DateTime of the execution.
    
    	- addl file is generating the domain definition line.

---
+++ problem-generator/problem-generator/src/main/java/problemGenerator/fileGenerator/PDDLGenerator.java
@@ -0,0 +1,13 @@
+package problemGenerator.fileGenerator;
+
+public class PDDLGenerator {
+
+	private static final String FOLDER_PATH = "./gen/pddl/";
+	private static final String FILE_TYPE = ".pddl";
+
+	private FileGenerator fg = new FileGenerator();
+
+	public  boolean generateFile(String fileName) {
+		return fg.generateFile(FOLDER_PATH, fileName, FILE_TYPE);
+	}
+}
