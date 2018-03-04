96b6a7cbe358bddea6fc86f0ec4be854d3a2126c
dan <dan@dan-VirtualBox>
2018-03-04 23:09:44 +0200

    initial commit for problem-generator project:
    * focuses on the logistics00 domain only!
    
    	- has addl + pddl file generated in the corresponding folders
    (./gen/addl + ./gen pddl).
    
    	- files are names by the DateTime of the execution.
    
    	- addl file is generating the domain definition line.

---
+++ problem-generator/problem-generator/src/main/java/problemGenerator/App.java
@@ -0,0 +1,90 @@
+package problemGenerator;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import problemGenerator.fileGenerator.ADDLGenerator;
+import problemGenerator.fileGenerator.PDDLGenerator;
+
+public class App 
+{
+	private static String timeStamp;
+	
+	private static ADDLGenerator addl;
+	private static PDDLGenerator pddl;
+
+	public static void main( String[] args )
+	{
+		/*
+		DefineProblemName();
+		DefineDomain();
+		DefineObjects();
+		DefineInit();
+		DefineGoal();
+		 */
+
+		printArgs(args);
+
+		checkArgs(args);
+		
+		setTimeStamp();
+		
+		initGenerators();
+
+		createProblemFile();
+		
+		createAgentFile();
+
+		/*
+        String[] a = {
+        		"cz.agents.madla.creator.MAPDDLCreator",
+        		"mapddl-benchmarks/logistics00/domain.pddl",
+        		"mapddl-benchmarks/logistics00/probLOGISTICS-4-0.pddl", 
+        		"benchmarks/logistics00/probLOGISTICS-4-0.addl",
+        		"PPsaFF-glcl",
+        		"-1",
+        		"10"
+        		};      
+
+        MAPDDLCreator c = new MAPDDLCreator();
+        c.init(a);
+        c.create();
+		 */
+	}
+
+	private static void initGenerators() {
+		addl = new ADDLGenerator();
+		pddl = new PDDLGenerator();
+	}
+
+	private static void setTimeStamp() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");	
+		timeStamp = dateFormat.format(Calendar.getInstance().getTime());
+	}
+
+	private static void checkArgs(String[] args) {
+		if (args.length != 1) {
+			System.out.println("provided args: " + Arrays.toString(args));
+			System.out.println("Usage ...");
+			System.exit(1);
+		}
+	}
+
+	private static void printArgs(String[] args) {
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+
+	private static void createAgentFile() {
+		addl.generateADDLFile(timeStamp);
+	}
+
+	private static void createProblemFile() {
+		pddl.generateFile(timeStamp);
+	}
+
+	
+}
---
+++ problem-generator/problem-generator/src/main/java/problemGenerator/fileGenerator/ADDLGenerator.java
@@ -0,0 +1,29 @@
+package problemGenerator.fileGenerator;
+
+public class ADDLGenerator{
+
+	private static final String FOLDER_PATH = "./gen/addl/";
+	private static final String FILE_TYPE = ".addl";
+
+	private FileGenerator fg = new FileGenerator();
+	private String fileName = null;
+
+	public boolean generateADDLFile(String fileName) {
+
+		this.fileName = fileName;
+
+		if(!generateFile(fileName))
+			return false;
+		if(!defineDomain())
+			return false;
+		return true;
+	}
+
+	private boolean defineDomain() {
+		return fg.WriteToFile("(define (problem "+fileName+")");
+	}
+
+	private boolean generateFile(String fileName) {
+		return fg.generateFile(FOLDER_PATH, fileName, FILE_TYPE);
+	}
+}
