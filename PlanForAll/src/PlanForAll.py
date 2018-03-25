589168d7af56f0342c38efc727d50f823c195209
dan <dan@dan-VirtualBox>
2018-03-26 01:05:36 +0300

    plan for all script

---
+++ PlanForAll/src/PlanForAll.py
@@ -0,0 +1,69 @@
+'''
+Created on Mar 25, 2018
+
+@author: dan
+'''
+
+import os
+import time
+import argparse
+import subprocess
+import shutil
+
+def main():
+    
+    start = time.time()
+
+    args = parse_args()
+    
+    for dirpath, dirnames, filenames in os.walk(args.problems):
+        for filename in filenames:
+            if filename.endswith(".pddl") :
+                #print(filename)
+                
+                agent = 'temp'
+
+                if os.path.exists(agent):
+                    shutil.rmtree(agent)
+                else:
+                    os.makedirs(agent)
+                
+                agent = str(agent +"/" + (os.path.splitext(filename))[0] +".addl")  
+                
+                subprocess.call(["./run_local.sh" 
+                                 , args.creator 
+                                 , args.domain
+                                 , os.path.join(args.problems, filename) 
+                                 , agent
+                                 , args.heuristic
+                                 , str(args.recursion)
+                                 , str(args.timeout) ])
+            
+    end = time.time() 
+    
+    print("Done! (time - %0.4f" %(end - start)+")")
+    
+
+def parse_args():
+    
+    argparser = argparse.ArgumentParser()
+    
+    argparser.add_argument(
+        "creator", help="creator class", type=str)
+    argparser.add_argument(
+        "domain", help="path to domain file", type=str)
+    argparser.add_argument(
+        "problems", help="path to problems folder", type=str)
+    argparser.add_argument(
+        "heuristic", help="heuristic", type=str)
+    argparser.add_argument(
+        "recursion", help="recursion level", type=int)
+    argparser.add_argument(
+        "timeout", help="timeout (min)", type=int)
+        
+    return argparser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
+    
