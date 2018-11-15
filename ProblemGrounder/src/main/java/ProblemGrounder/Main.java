a3ae4d7460b38bb7fd1ce4c47e8ab1cf510218c4
daniel <daniel@daniel-Latitude-7490>
2018-11-15 22:27:29 +0200

    Initial Commit
    Create project folders and files
    Setup Maven files (pom,snapshots)
    Setup log4j logging
    Add arguments validation method
    Add argument file paths validation method
    Add Agent file creation method
    Setup PyDev support

---
+++ ProblemGrounder/src/main/java/ProblemGrounder/Main.java
@@ -0,0 +1,10 @@
+package ProblemGrounder;
+
+import cz.agents.alite.creator.CreatorFactory;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		CreatorFactory.createCreator(args).create();
+	}
+}
