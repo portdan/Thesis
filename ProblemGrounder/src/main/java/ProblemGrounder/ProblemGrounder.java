d6db853f972ece4c32fb4feda209c545193ccf3c
daniel <daniel@daniel-Latitude-7490>
2018-11-30 17:53:15 +0200

    Grounding Problem v.1.0
    Add python script for generating agent file
    Add python script for grounding the problem
    Add run configurations for 'logistics' and 'rovers' domains

--- ProblemGrounder/src/main/java/ProblemGrounder/ProblemGrounder.java
+++ ProblemGrounder/src/main/java/ProblemGrounder/ProblemGrounder.java
@@ -12,24 +12,29 @@ public class ProblemGrounder implements Creator {
 
 	/* Global variables */
 	private final static Logger LOGGER = Logger.getLogger(ProblemGrounder.class);
-	private final static int ARGS_NUM = 4;
-	
+	private final static int ARGS_NUM = 3;
+
 	private static final String MA_TO_AGENT_FILE_SCRIPT = "./PythonScripts/generate-agent-file.py";
-	private static final String MA_TO_AGENT_FILE_OUTPUT = "./Output/Agents";
+	private static final String MA_TO_AGENT_FILE_OUTPUT = "./Output/Agent";
+	private static final String GROUND_PROBLEM_SCRIPT = "./PythonScripts/ground-problem.py";
+	private static final String GROUND_PROBLEM_OUTPUT = "./Output/Grounded";
+	private static final String LOCAL_VIEW_OUTPUT = "./Output/LocalView";
 
 	/* Class variables */
-	private String tracePath = "";
+	//private String tracePath = "";
 	private String domainPath = "";
 	private String problemPath = "";
-	
-	private File traceFile = null;
+	private String agentsPath = "";
+
+	//private File traceFile = null;
 	private File domainFile = null;
 	private File problemFile = null;
+	private File agentsFile = null;
 
 	@Override
 	public void create() {
 
-		LOGGER.info("ProblemGrounder End");
+		LOGGER.info("ProblemGrounder end");
 
 	}
 	@Override
@@ -37,40 +42,35 @@ public class ProblemGrounder implements Creator {
 
 		Trace.setFileStream("Log/trace.log");
 
-		LOGGER.info("ProblemGrounder Start");
+		LOGGER.info("ProblemGrounder start");
 
 		if(!ParseArgs(args))
 		{
 			LOGGER.fatal("Args not valid");
 			System.exit(1);
 		}
-		
+
 		if(!CreateAgentsFile(MA_TO_AGENT_FILE_OUTPUT))
 		{
 			LOGGER.fatal("Agents file creation failure");
 			System.exit(1);
 		}
-		
-		if(!ReadTraceFile(tracePath))
+
+		if(!GroundProblem(GROUND_PROBLEM_OUTPUT,LOCAL_VIEW_OUTPUT))
 		{
-			LOGGER.fatal("Trace file reading failure");
+			LOGGER.fatal("Domain file grounding failure");
 			System.exit(1);
 		}
 	}
 
 
-	private boolean ReadTraceFile(String traceFilePath) {
+	private boolean GroundProblem(String outputPath, String localViewOutput) {
 
-		return false;
-	}
-	
-	private boolean CreateAgentsFile(String outputPath) {
-
-		LOGGER.info("Agents File creation");
+		LOGGER.info("Grounding problem ");
 
 		try {
 
-			String cmd = MA_TO_AGENT_FILE_SCRIPT + " " + domainPath + " " + problemPath + " " + outputPath;
+			String cmd = GROUND_PROBLEM_SCRIPT + " " + domainPath + " " + problemPath + " " + agentsPath + " " + outputPath + " " + localViewOutput;
 
 			LOGGER.info("Running: " + cmd);
 
@@ -82,10 +82,51 @@ public class ProblemGrounder implements Creator {
 			LOGGER.fatal(e, e);
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
+	private boolean CreateAgentsFile(String outputPath) {
+
+		LOGGER.info("Agents file creation");
+
+		try {
+
+			String cmd = MA_TO_AGENT_FILE_SCRIPT + " " + domainPath + " " + problemPath + " " + outputPath;
+
+			LOGGER.info("Running: " + cmd);
+
+			Process pr = Runtime.getRuntime().exec(cmd);
+
+			pr.waitFor();
+
+		} 
+		catch (Exception e) {
+			LOGGER.fatal(e, e);
+			return false;
+		}
+
+		return agentsFileValid(outputPath);
+	}
+	private boolean agentsFileValid(String outputPath) {
+
+		LOGGER.info("Agents file check");	
+		
+		String problemName = problemFile.getName();
+		
+		problemName = problemName.substring(0, problemName.lastIndexOf('.'));
+
+		agentsPath = outputPath + "/" + problemName + ".agents";
+		agentsFile = new File(agentsPath);
+
+		if( !agentsFile.exists()) {
+			LOGGER.fatal("agents file generation failure");
+			return false;
+		}
+
+		return true;
+	}
+
 	private boolean ParseArgs(String[] args) {
 
 		LOGGER.info("Args validity check");
@@ -104,7 +145,7 @@ public class ProblemGrounder implements Creator {
 			LOGGER.fatal(status);
 			return false;
 		}
-		
+
 		return true;
 	}
 
@@ -118,31 +159,32 @@ public class ProblemGrounder implements Creator {
 	private boolean ArgsParsingValid(String[] args) {
 
 		LOGGER.info("Args parse check");
-		
+
 		domainPath = args[1];
 		domainFile = new File(domainPath);
-		
+
 		if( !domainFile.exists()) {
 			LOGGER.fatal("provided path to domain file not existing");
 			return false;
 		}
-		
+
 		problemPath = args[2];
 		problemFile = new File(problemPath);
-		
+
 		if( !problemFile.exists()) {
 			LOGGER.fatal("provided path to problem file not existing");
 			return false;
 		}
 
+		/*
 		tracePath = args[3];
 		traceFile = new File(tracePath);
-		
+
 		if( !traceFile.exists()) {
 			LOGGER.fatal("provided path to trace file not existing");
 			return false;
-		}
-		
+		}		
+		 */
 		return true;
 	}
 }
