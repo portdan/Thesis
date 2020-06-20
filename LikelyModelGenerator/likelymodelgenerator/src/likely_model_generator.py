b5131a4ea97e9af33ad80da0b18d6bab3fb05aba
portdan <portdan@post.bgu.ac.il>
2020-06-20 22:51:58 +0300

    LikelyModelGenerator project:
    - config file reworked
    - save Data class with safe model, counts, deltas and uses count

--- LikelyModelGenerator/likelymodelgenerator/src/likely_model_generator.py
+++ LikelyModelGenerator/likelymodelgenerator/src/likely_model_generator.py
@@ -3,7 +3,6 @@ import likelymodelgenerator.src.config_reader as cr
 
 from sklearn.feature_extraction.text import TfidfTransformer
 import time
-from sklearn.cluster import KMeans
 import numpy as np
 import _collections
 
@@ -14,8 +13,7 @@ def dummy(s):
 
 def run(args):
     config_path = args.config
-
-    config = cr.read_config(config_path)
+    config = cr.Configuration(config_path)
 
     # start = time.clock()
     # df = dp.pre_process_data_csv(data_file, pre_processed_file_csv)
@@ -33,48 +31,34 @@ def run(args):
     # tfidf_transformer = TfidfTransformer()
     # tfidf = tfidf_transformer.fit_transform(count)
 
-    start = time.clock()
+    if config.preprocess_traces:
+        start = time.clock()
+        data = dp.preprocess_data(config)
+        print("\npreprocess_data time: ", time.clock() - start)
 
-    if config.preprocess:
-        if config.preprocessed_read_format == "pkl":
-            actions_preprocessed, preconditions_preprocessed = dp.pre_process_data_pkl(config)
-        elif config.preprocessed_read_format == "csv":
-            actions_preprocessed, preconditions_preprocessed = dp.pre_process_data_csv(config)
+    if config.load_data:
+        start = time.clock()
+        data = dp.load_preprocessed_data(config)
+        print("\nload_preprocessed_data time: ", time.clock() - start)
 
-    print("\npre_process_data time: ", time.clock() - start)
-
-    start = time.clock()
-
-    if config.load_preprocessed:
-        if config.preprocessed_read_format == "pkl":
-            actions_loaded, preconditions_loaded = dp.load_pre_process_data_pkl(config)
-        elif config.preprocessed_read_format == "csv":
-            actions_loaded, preconditions_loaded = dp.load_pre_process_data_csv(config)
-
-    print("\nload_pre_process_data time: ", time.clock() - start)
-
-    start = time.clock()
-
-    if config.update_preprocessed:
-        if config.preprocessed_read_format == "pkl":
-            actions_loaded, preconditions_loaded = dp.pre_process_append_new_traces_pkl(config, [
-                'StateActionState [ pre[ b ]  ; action[ asd ]  '
-                '; actionOwner[ satellite0 ]  ; post[ a ]  ; '
-                'traceNum[ 438 ] ]'])
-        elif config.preprocessed_read_format == "csv":
-            actions_loaded, preconditions_loaded = dp.pre_process_append_new_traces_csv(config, [
-                'StateActionState [ pre[ b ]  ; action[ asd ]  '
-                '; actionOwner[ satellite0 ]  ; post[ a ]  ; '
-                'traceNum[ 438 ] ]'])
-
-    print("\npre_process_append_new_traces time: ", time.clock() - start)
+    if config.update_data:
+        start = time.clock()
+        data = dp.update_preprocessed_data(data, [
+            'StateActionState [ pre[ at(tru1, pos1) ; in(obj1, tru1) ; at(apn1, apt1) ]  ; '
+            'action[ unload-truck-agent-tru1 tru1 obj1 pos1 ]  ; '
+            'actionOwner[ tru1 ]  ; '
+            'post[ at(tru1, pos1) ; at(obj1, pos1) ; at(apn1, apt1) ]  ; '
+            'traceNum[ 2 ] ]'
+        ],
+                                           'StateActionState [ pre[ at(tru1, pos1) ; in(obj1, tru1) ; at(apn1, apt1) ]  ; '
+                                           'action[ unload-truck-agent-tru1 tru1 obj1 pos1 ]  ; '
+                                           'actionOwner[ tru1 ]  ; '
+                                           'post[ at(tru1, pos1) ; at(obj1, pos1) ; at(apn1, apt1) ]  ; '
+                                           'traceNum[ 2 ] ]')
+        print("\nupdate_preprocessed_data time: ", time.clock() - start)
 
     start = time.clock()
-    vocabulary = dp.get_vocabulary(preconditions_loaded)
-    print("\nget_vocabulary time: ", time.clock() - start)
-
-    start = time.clock()
-    counts = dp.get_counts_as_arrays(actions_loaded, vocabulary.keys())
+    counts = dp.get_actions_preconditions_counts_as_arrays(data)
     tfidf_transformer = TfidfTransformer()
     tfidf = tfidf_transformer.fit_transform(counts)
     print("\ntfidf_transformer fit_transform time: ", time.clock() - start)
@@ -82,17 +66,12 @@ def run(args):
     asd = tfidf.toarray()
     asdasd = _collections.OrderedDict()
 
-    for l in range(0, len(actions_loaded)):
+    for l in range(0, len(data.actions)):
         max = np.max(asd[l])
-        aaa = np.argwhere(asd[l] > 0.8 * max)
+        indices = np.argwhere(asd[l] >= 0.8 * max).flatten()
+        aaa = [data.preconditions[i] for i in indices]
 
-        bbb = []
-        for pre, index in vocabulary.items():
-            for i in aaa:
-                if i == index:
-                    bbb.append(pre)
-
-        asdasd[list(actions_loaded.keys())[l]] = bbb
+        asdasd[data.actions[l]] = aaa
 
     # a = [[v] for v in asd[2] ]
     # kmeans = KMeans(n_clusters=5, random_state=0).fit(a)
